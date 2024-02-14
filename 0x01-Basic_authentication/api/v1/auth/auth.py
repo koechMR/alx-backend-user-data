feb0285523b638352ab02a0ab37f379600cc9191
koechMR <briankipkoech328@gmail.com>
2024-02-14 18:49:49 +0300

    new

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
-"""Authentication module.
+"""Auth route module for the API
 """
-from flask import request
 from typing import List, TypeVar
-import fnmatch
+from flask import request
+from itsdangerous import exc
 
 
 class Auth:
-    """Authentication class.
-    """
+    """API Authentication class"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if auth is required.
-        """
-        if path is None:
+        """Check if a route path requires auth"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-
-        if excluded_paths is None or not excluded_paths:
+        if excluded_paths is None or len(excluded_paths) == 0:
             return True
-
         for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
+            if path.rstrip("/") == excluded_path.rstrip("/"):
                 return False
-
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Method to get authorization header.
-        """
-        if request is not None:
-            return request.headers.get('Authorization', None)
-        return None
+        """Authorization header"""
+        if request is None or request.headers.get("Authorization") is None:
+            return None
+        else:
+            return request.headers.get("Authorization")
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ Method to get user from request.
-        """
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Return current user for a request"""
         return None
