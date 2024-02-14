25a297134cb2ebe4d840b5065092f1b8e502ef0f
koechMR <briankipkoech328@gmail.com>
2024-02-14 18:48:08 +0300

    new

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
-"""Auth route module for the API
+"""Authentication module.
 """
-from typing import List, TypeVar
 from flask import request
-from itsdangerous import exc
+from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
-    """API Authentication class"""
-
+    """Authentication class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Check if a route path requires auth"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        """ Method to check if auth is required.
+        """
+        if path is None:
             return True
-        if excluded_paths is None or len(excluded_paths) == 0:
+
+        if excluded_paths is None or not excluded_paths:
             return True
+
         for excluded_path in excluded_paths:
-            if path.rstrip("/") == excluded_path.rstrip("/"):
+            if fnmatch.fnmatch(path, excluded_path):
                 return False
-            if excluded_path.endswith('*'):
-                if path.startswith(excluded_path[:-1]):
-                    return False
-            elif path == excluded_path:
-                return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Authorization header"""
-        if request is None or request.headers.get("Authorization") is None:
-            return None
-        else:
-            return request.headers.get("Authorization")
+        """ Method to get authorization header.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
 
-    def current_user(self, request=None) -> TypeVar("User"):
-        """Return current user for a request"""
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method to get user from request.
+        """
         return None
